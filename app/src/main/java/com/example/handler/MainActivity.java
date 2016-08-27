f3f7c99e95666e9d87462bec6fb02a3e5818a43b
Evin1- <edwin.hdz.12.0@gmail.com>
2016-08-26 22:10:17 -0400

    Add Handler logic

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.handler;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +11,25 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTAG_";
 
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                Log.d(TAG, "handleMessage: " + msg.what);
+                Log.d(TAG, "handleMessage: " + Thread.currentThread());
+            }
+        };
+
+        MyThread myThread = new MyThread(mHandler);
+        myThread.start();
+
+        Log.d(TAG, "onCreate: " + Thread.currentThread());
     }
 }
--- app/src/main/java/com/example/handler/MyThread.java
+++ app/src/main/java/com/example/handler/MyThread.java
@@ -1,15 +1,23 @@
 package com.example.handler;
 
+import android.os.Handler;
 import android.util.Log;
 
 /**
  * Created by evin on 8/26/16.
  */
-public class MyThread extends Thread{
+public class MyThread extends Thread {
     private static final String TAG = "MyThreadTAG_";
+
+    private Handler mHandler;
+
+    public MyThread(Handler handler) {
+        mHandler = handler;
+    }
 
     @Override
     public void run() {
-        Log.d(TAG, "run: ");
+        Log.d(TAG, "run: " + Thread.currentThread());
+        mHandler.sendEmptyMessage(1);
     }
 }
